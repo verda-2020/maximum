65fffd0eb4b22b6241af549e0b8c7fe8c45c8822
verda-2020 <msufuk@gmail.com>
2022-03-04 05:45:18 -0500

    commit maximum

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random rd = new Random();
+        int num1 = 5;
+        int num2 = 9;
+
+        System.out.println(Math.max(num1,num2));
+        System.out.println(Math.min(num1,num2));
+    }
+}
